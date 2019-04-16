aec98cc5562b6e2ea05344d328f9624199ccccbc
wldls <dslet2343@gmail.com>
2019-04-16 18:02:35 +0900

    file delete

--- dadmin/dbinfo.php
+++
@@ -1,5 +0,0 @@
-<?php
-	$link = mysql_connect('localhost', 'devmon', 'Devmon.kr:8080')
-	or die('Could not connect: '. mysql_error());
-	mysql_select_db('DEVMON_INQUIRY');
-?>
--- dadmin/js/admfoo.js
+++
@@ -1,6 +0,0 @@
-var ADMBTM='';
-ADMBTM += '  <div class="admfoo">';
-ADMBTM += '		<p>COPYTIRIGHT. DEVMON CO., LTD. ALL RIGHTS RESERVED</p>';
-ADMBTM += '	 </div>';
-
-document.write(ADMBTM);
